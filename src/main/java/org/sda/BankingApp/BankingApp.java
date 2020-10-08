e60afb1a6b6a67016db783b230333a033c5dd0c5
Helfried <helfried.git@gmail.com>
2020-10-08 20:12:14 +0300

    commit

--- src/main/java/org/sda/BankingApp/BankingApp.java
+++ src/main/java/org/sda/BankingApp/BankingApp.java
@@ -1,3 +1,5 @@
+package org.sda.BankingApp;
+
 public class BankingApp {
 
 }
---
+++ src/main/java/org/sda/BankingApp/config/HibernateUtil.java
@@ -0,0 +1,39 @@
+package org.sda.BankingApp.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+import static java.util.logging.Level.OFF;
+
+public class HibernateUtil {
+
+    public static SessionFactory getSessionFactory() {
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(OFF);
+        Configuration configuration = createConfig();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+            .applySettings(configuration.getProperties()).build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    private static Configuration createConfig() {
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/banking?serverTimezone=UTC");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "password");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        settings.put(Environment.SHOW_SQL, "false");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        configuration.setProperties(settings);
+
+        configuration.addAnnotatedClass(Client.class);//
+
+        return configuration;
+    }
+}
