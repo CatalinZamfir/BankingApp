bc5dc54e3329fe5fdab81d78fe62651c3f911eb6
Helfried <helfried.git@gmail.com>
2020-10-12 18:36:33 +0300

    commit

--- src/main/resources/bank_data.sql
+++ src/main/resources/bank_data.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `bank_data`;
 USE `bank_data`;
 
+DROP TABLE IF EXISTS `transactions`;
+DROP TABLE IF EXISTS `account`;
 DROP TABLE IF EXISTS `client_data`;
 
 CREATE TABLE `client_data` (
@@ -13,19 +15,17 @@ CREATE TABLE `client_data` (
   PRIMARY KEY (`username`)
 );
 
-DROP TABLE IF EXISTS `accounts`;
-
 CREATE TABLE `account` (
-  `account_no` int(8) NOT NULL AUTO_INCREMENT = 10000001,
+  `account_no` int(8) NOT NULL AUTO_INCREMENT,
   `username` varchar(15) NOT NULL,
   `account_type` int(1) NOT NULL,
   `balance` int,
   `currency` int(1) NOT NULL,
   PRIMARY KEY (`account_no`),
-  FOREIGN KEY (`username`) REFERENCES client_data(`username`),
+  FOREIGN KEY (`username`) REFERENCES client_data(`username`)
 );
 
-DROP TABLE IF EXISTS `transactions`;
+ALTER TABLE `account` AUTO_INCREMENT = 10000001;
 
 CREATE TABLE `transactions` (
   `reference_id` int(8) NOT NULL AUTO_INCREMENT,
@@ -33,7 +33,7 @@ CREATE TABLE `transactions` (
   `account_no` int(8) NOT NULL,
   `transfer_amount` int,
   `foreign_account` varchar(34) NOT NULL,
-  `date_and_time` date NOT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`reference_id`),
-  FOREIGN KEY (`account_no`) REFERENCES accounts(`account_no`)
+  FOREIGN KEY (`account_no`) REFERENCES account(`account_no`)
 );
